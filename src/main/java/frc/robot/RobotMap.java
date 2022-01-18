9ed9a912197db1d045b68f391ab44349c27ad524
OfriRosenbaum <ofriro1000>
2022-01-18 18:37:34 +0200

    created Intake class

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,4 +1,16 @@
 package frc.robot;
 
 public class RobotMap {
+
+    public interface CAN {
+
+        int INTAKE_PLACER = -1;
+        int INTAKE_ROLLER = -1;
+    }
+
+    public interface DIO{
+
+        int INTAKE_UP_LIMIT = -1;
+        int INTAKE_DOWN_LIMIT = -1;
+    }
 }
--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,4 +1,53 @@
 package frc.robot.Subsystems;
 
-public class Intake {
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.spikes2212.command.genericsubsystem.GenericSubsystem;
+import com.spikes2212.dashboard.RootNamespace;
+import frc.robot.RobotMap;
+
+public class Intake extends GenericSubsystem {
+
+    private static final double MAX_SPEED = 0.5;
+    private static final double MIN_SPEED = -0.5;
+
+    private static Intake instance;
+    private WPI_VictorSPX placer;
+    private WPI_VictorSPX roller;
+
+    public static final RootNamespace intakeNamespace = new RootNamespace("intake");
+
+    public static Intake getInstance() {
+        if (instance == null) {
+            instance = new Intake();
+        }
+        return instance;
+    }
+
+    private Intake() {
+        super(MIN_SPEED, MAX_SPEED);
+        placer = new WPI_VictorSPX(RobotMap.CAN.INTAKE_PLACER);
+        roller = new WPI_VictorSPX(RobotMap.CAN.INTAKE_ROLLER);
+    }
+
+    @Override
+    protected void apply(double speed) {
+        placer.set(speed);
+        roller.set(speed);
+    }
+
+    @Override
+    public boolean canMove(double speed) {
+        return true;
+    }
+
+    @Override
+    public void stop() {
+        placer.stopMotor();
+        roller.stopMotor();
+    }
+
+    @Override
+    public void periodic() {
+    intakeNamespace.update();
+    }
 }
