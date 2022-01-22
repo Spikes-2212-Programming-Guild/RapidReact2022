f44cc562612cd1169663c7419317d8de0775cd89
OfriRosenbaum <ofriro1000>
2022-01-22 18:04:43 +0200

    fixed OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,15 +1,15 @@
 package frc.robot;
 
-import com.spikes2212.util.XboXUID;
+import com.spikes2212.util.XboxControllerWrapper;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import frc.robot.Commands.IntakeCargo;
 
 public class OI /* GEVALD */ {
 
-    private XboXUID controller = new XboXUID(0);
+    private XboxControllerWrapper controller = new XboxControllerWrapper(0);
 
     public OI() {
-        Button intake = controller.getRBButton();
+        Button intake = controller.getGreenButton();
         intake.whenPressed(new IntakeCargo());
     }
 }
