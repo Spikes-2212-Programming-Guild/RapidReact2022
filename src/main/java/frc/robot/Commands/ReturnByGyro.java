98a86bd011c11797b6ba9987eab9b91299ab3a1d
OfriRosenbaum <ofriro1000>
2022-01-22 10:11:35 +0200

    created classes

---
+++ src/main/java/frc/robot/Commands/ReturnByGyro.java
@@ -0,0 +1,6 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ReturnByGyro extends CommandBase {
+}
---
+++ src/main/java/frc/robot/Subsystems/Dispenser.java
@@ -0,0 +1,20 @@
+package frc.robot.Subsystems;
+
+import com.spikes2212.util.Limelight;
+
+public class Dispenser {
+
+    private Limelight limelight;
+
+    private Dispenser(){
+        this.limelight = new Limelight();
+    }
+
+    public static Dispenser getInstance() {
+        return null;
+    }
+
+    public boolean isOnTarget(){
+        return limelight.isOnTarget();
+    }
+}
