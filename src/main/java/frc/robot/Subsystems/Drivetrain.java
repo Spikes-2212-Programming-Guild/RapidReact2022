2bb48a5cf156b5726b87c1ba576ebd2149600769
yotam <yotamyizhar@gmail.com>
2022-01-23 19:41:36 +0200

    added color sensors

--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -1,9 +1,11 @@
 package frc.robot.Subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.ColorSensorV3;
 import com.spikes2212.command.drivetrains.TankDrivetrain;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.RobotMap;
 
@@ -12,6 +14,7 @@ public class Drivetrain extends TankDrivetrain {
 
     private ADXRS450_Gyro gyro;
     private Encoder leftEncoder, rightEncoder;
+    private ColorSensorV3 colorSensor1, colorSensor2;
 
     private Drivetrain() {
         super(new MotorControllerGroup(new WPI_VictorSPX(RobotMap.CAN.DRIVETRAIN_LEFT_VICTOR_1), new WPI_VictorSPX(RobotMap.CAN.DRIVETRAIN_LEFT_VICTOR_2)),
@@ -20,6 +23,8 @@ public class Drivetrain extends TankDrivetrain {
         this.gyro = new ADXRS450_Gyro();
         this.leftEncoder = new Encoder(RobotMap.CAN.DRIVETRAIN_LEFT_ENCODER_POS, RobotMap.CAN.DRIVETRAIN_LEFT_ENCODER_NEG);
         this.rightEncoder = new Encoder(RobotMap.CAN.DRIVETRAIN_RIGHT_ENCODER_POS, RobotMap.CAN.DRIVETRAIN_RIGHT_ENCODER_NEG);
+        this.colorSensor1 = new ColorSensorV3(I2C.Port.kOnboard);
+        this.colorSensor2 = new ColorSensorV3(I2C.Port.kMXP);
     }
 
     public static Drivetrain getInstance() {
