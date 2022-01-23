08e133872f729d2d35ec0daf49287507f83577f0
yotam <yotamyizhar@gmail.com>
2022-01-23 18:53:39 +0200

    added initial transfer subsystem

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,4 +1,12 @@
 package frc.robot;
 
 public class RobotMap {
+    public interface CAN {
+        int TRANSFER_TIMING_BELT_TALON = -1;
+    }
+
+    public interface DIO {
+        int TRANSFER_ENTRANCE_LIGHT_SENSOR = -1;
+        int TRANSFER_EXIT_LIMIT_SWITCH = -1;
+    }
 }
--- src/main/java/frc/robot/Subsystems/Transfer.java
+++ src/main/java/frc/robot/Subsystems/Transfer.java
@@ -1,4 +1,44 @@
 package frc.robot.Subsystems;
 
-public class Transfer {
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.spikes2212.command.genericsubsystem.GenericSubsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.RobotMap;
+
+public class Transfer extends GenericSubsystem {
+    WPI_TalonSRX talon;
+    DigitalInput entranceSensor;
+    DigitalInput exitSensor;
+
+    private static Transfer instance;
+
+    private Transfer() {
+        super();
+        this.talon = new WPI_TalonSRX(RobotMap.CAN.TRANSFER_TIMING_BELT_TALON);
+        this.entranceSensor = new DigitalInput(RobotMap.DIO.TRANSFER_ENTRANCE_LIGHT_SENSOR);
+        this.exitSensor = new DigitalInput(RobotMap.DIO.TRANSFER_EXIT_LIMIT_SWITCH);
+    }
+
+    public static Transfer getInstance() {
+        if (instance == null) {
+            instance = new Transfer();
+        }
+
+        return instance;
+    }
+
+    @Override
+    protected void apply(double speed) {
+        talon.set(speed);
+    }
+
+    @Override
+    public boolean canMove(double speed) {
+        return true;
+    }
+
+    @Override
+    public void stop() {
+        talon.stopMotor();
+    }
 }
