74d8cf7be64dde09303200bb5d718274f17ddd6e
barbenhamou <barbenhamou2006@gmail.com>
2022-01-25 22:57:06 +0200

    bar - climber fixed

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,4 +1,12 @@
 package frc.robot;
 
 public class RobotMap {
+    public interface CAN{
+        int CLIMBER_VICTOR_RIGHT=-1;
+        int CLIMBER_VICTOR_LEFT=-1;
+    }
+
+    public interface DIO{
+        int CLIMBER_HALL_EFFECT=-1;
+    }
 }
--- src/main/java/frc/robot/Subsystems/Climber.java
+++ src/main/java/frc/robot/Subsystems/Climber.java
@@ -5,50 +5,50 @@ import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 
-public class Climber extends MotoredGenericSubsystem{
-    private WPI_VictorSPX rightVictor;
-    private WPI_VictorSPX leftVictor;
-    private static final double MINSPEED=-0.6;
-    private static final double MAXSPEED=-0.6;
-    private static boolean counted=false;
-    private static int magnets=0;
+public class Climber extends MotoredGenericSubsystem {
+    private static final double MIN_SPEED = -0.6;
+    private static final double MAX_SPEED = -0.6;
+
+    private static Climber instance;
+    private static boolean counted = false;
+    private static int magnets = 0;
     private static DigitalInput digitalInput;
 
 
-    public Climber(WPI_VictorSPX rightVictor, WPI_VictorSPX leftVictor, DigitalInput digitalInput){
-        super(MINSPEED,MAXSPEED,"climber",rightVictor,leftVictor);
-        this.rightVictor=rightVictor;
-        this.leftVictor=leftVictor;
+    public static Climber getInstance() {
+        if (instance == null) {
+            instance = new Climber(new WPI_VictorSPX(-1), new WPI_VictorSPX(-1), new DigitalInput(-1));
+        }
+        return instance;
+    }
+
+    public Climber(WPI_VictorSPX rightVictor, WPI_VictorSPX leftVictor, DigitalInput digitalInput) {
+        super(MIN_SPEED, MAX_SPEED, "climber", rightVictor, leftVictor);
         leftVictor.follow(rightVictor);
-        this.digitalInput=digitalInput;
+        this.digitalInput = digitalInput;
     }
 
     @Override
     public void apply(double speed) {
-        rightVictor.set(speed);
-        if(counted&&digitalInput.get())
-            counted=false;
-        }
+        super.apply(speed);
+        if (counted && digitalInput.get())
+            counted = false;
+    }
 
     @Override
     public boolean canMove(double speed) {
-        if(!counted&&digitalInput.get()){
-            if(speed>0)
+        if (!counted && digitalInput.get()) {
+            if (speed > 0)
                 magnets++;
             else
                 magnets--;
         }
-        if(magnets==2&&speed<0)
+        if (magnets == 2 && speed < 0)
             return true;
-        if(magnets==0&&speed>0)
+        if (magnets == 0 && speed > 0)
             return true;
         return false;
-
     }
 
-    @Override
-    public void stop() {
-        rightVictor.stopMotor();
-}
 
 }
