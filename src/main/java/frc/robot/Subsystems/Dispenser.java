edc1ebfd5b27b86d9caa811a2a7e28c9e4713631
yotam <yotamyizhar@gmail.com>
2022-01-26 19:35:04 +0200

    removed distance per pulse

--- src/main/java/frc/robot/Subsystems/Dispenser.java
+++ src/main/java/frc/robot/Subsystems/Dispenser.java
@@ -7,8 +7,6 @@ import frc.robot.RobotMap;
 
 public class Dispenser extends MotoredGenericSubsystem {
 
-    private static final double DISTANCE_PER_PULSE = 20.32 / 4096.0;
-
     private Encoder encoder;
 
     private static Dispenser instance;
@@ -23,12 +21,10 @@ public class Dispenser extends MotoredGenericSubsystem {
     private Dispenser(WPI_TalonSRX talon) {
         super("dispenser", talon);
         this.encoder = new Encoder(RobotMap.DIO.DISPENSER_ENCODER_POS, RobotMap.DIO.DISPENSER_ENCODER_NEG);
-        this.encoder.setDistancePerPulse(DISTANCE_PER_PULSE);
     }
 
     @Override
     public void configureDashboard() {
-        rootNamespace.putNumber("encoder ticks", encoder::get);
-        rootNamespace.putNumber("encoder distance", encoder::getDistance);
+        rootNamespace.putNumber("encoder", encoder::get);
     }
 }
