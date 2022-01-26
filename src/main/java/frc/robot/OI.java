9f6c52f8cbc77613749241b9c732e0373b558e51
yotam <yotamyizhar@gmail.com>
2022-01-26 18:12:45 +0200

    fixed formatting and added data to dashboard

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,10 +6,6 @@ public class OI /* GEVALD */ {
     private Joystick left = new Joystick(0);
     private Joystick right = new Joystick(1);
 
-    public OI() {
-
-    }
-
     public double getRightY() {
         return right.getY();
     }
--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -3,10 +3,12 @@ package frc.robot.Subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.ColorSensorV3;
 import com.spikes2212.command.drivetrains.TankDrivetrain;
+import com.spikes2212.dashboard.RootNamespace;
 import com.spikes2212.util.PigeonWrapper;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.RobotMap;
 import frc.robot.utils.BustedMotorControllerGroup;
 
@@ -15,11 +17,11 @@ public class Drivetrain extends TankDrivetrain {
 
     private static Drivetrain drivetrain;
 
+    private final RootNamespace root = new RootNamespace("drivetrain");
+
     private PigeonWrapper pigeon;
     private Encoder leftEncoder, rightEncoder;
     private ColorSensorV3 rightColorSensor, leftColorSensor;
-    private MotorControllerGroup leftMotors;
-    private BustedMotorControllerGroup rightMotors;
 
     public static Drivetrain getInstance() {
         if (drivetrain == null) {
@@ -40,12 +42,33 @@ public class Drivetrain extends TankDrivetrain {
     private Drivetrain(MotorControllerGroup leftMotors, BustedMotorControllerGroup rightMotors) {
         super(leftMotors, rightMotors);
 
-        this.leftMotors = leftMotors;
-        this.rightMotors = rightMotors;
         this.pigeon = new PigeonWrapper(RobotMap.CAN.DRIVETRAIN_PIGEON);
         this.leftEncoder = new Encoder(RobotMap.DIO.DRIVETRAIN_LEFT_ENCODER_POS, RobotMap.DIO.DRIVETRAIN_LEFT_ENCODER_NEG);
         this.rightEncoder = new Encoder(RobotMap.DIO.DRIVETRAIN_RIGHT_ENCODER_POS, RobotMap.DIO.DRIVETRAIN_RIGHT_ENCODER_NEG);
+        this.leftEncoder.setDistancePerPulse(20.32 / 4096.0);
+        this.rightEncoder.setDistancePerPulse(20.32 / 4096.0);
         this.rightColorSensor = new ColorSensorV3(I2C.Port.kOnboard);
         this.leftColorSensor = new ColorSensorV3(I2C.Port.kMXP);
+
+        root.putNumber("left encoder ticks", this.leftEncoder::get);
+        root.putNumber("right encoder ticks", this.rightEncoder::get);
+        root.putNumber("left encoder distance", this.leftEncoder::getDistance);
+        root.putNumber("right encoder distance", this.rightEncoder::getDistance);
+
+        root.putNumber("left color sensor red", () -> this.getLeftColor().red);
+        root.putNumber("left color sensor blue", () -> this.getLeftColor().blue);
+        root.putNumber("left color sensor green", () -> this.getLeftColor().green);
+
+        root.putNumber("right color sensor red", () -> this.getRightColor().red);
+        root.putNumber("right color sensor blue", () -> this.getRightColor().blue);
+        root.putNumber("right color sensor green", () -> this.getRightColor().green);
+    }
+
+    public Color getLeftColor() {
+        return leftColorSensor.getColor();
+    }
+
+    public Color getRightColor() {
+        return rightColorSensor.getColor();
     }
 }
