befeb1c82e773c064f4c7d2322557db6234186ca
yotam <yotamyizhar@gmail.com>
2022-01-27 18:51:05 +0200

    initial commit

---
+++ src/main/java/frc/robot/Commands/DriveUntilHitHub.java
@@ -0,0 +1,33 @@
+package frc.robot.Commands;
+
+import com.spikes2212.command.drivetrains.commands.DriveArcade;
+import frc.robot.Subsystems.Drivetrain;
+
+import java.util.function.Supplier;
+
+/**
+ * drive the robot forward until it hits the hub. Robot needs to be pre aligned to the hub.
+ */
+public class DriveUntilHitHub extends DriveArcade {
+
+    // A value between the stall current and running current of the motors
+    private final Supplier<Double> STALL_CURRENT;
+
+    private final Drivetrain drivetrain;
+
+    public DriveUntilHitHub(Drivetrain drivetrain, Supplier<Double> movementSpeed, Supplier<Double> stallCurrent) {
+        super(drivetrain, movementSpeed, () -> 0.0);
+        this.drivetrain = drivetrain;
+        this.STALL_CURRENT = stallCurrent;
+    }
+
+    public DriveUntilHitHub(Drivetrain drivetrain, double movementSpeed, double stallCurrent) {
+        this(drivetrain, () -> movementSpeed, () -> stallCurrent);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return drivetrain.getRightTalon1Current() > STALL_CURRENT.get() &&
+                drivetrain.getLeftTalon1Current() > STALL_CURRENT.get();
+    }
+}
