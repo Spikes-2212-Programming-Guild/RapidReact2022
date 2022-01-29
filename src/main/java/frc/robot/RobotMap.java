c69a0afc3593203734833ef7dd10330958b8d8cb
yotam <yotamyizhar@gmail.com>
2022-01-29 18:22:44 +0200

    updated drivetrain

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,15 +1,18 @@
 package frc.robot;
 
 public class RobotMap {
+
     public interface CAN {
-        int DRIVETRAIN_LEFT_VICTOR_1 = 1;
-        int DRIVETRAIN_LEFT_VICTOR_2 = 3;
-        int DRIVETRAIN_RIGHT_VICTOR_1 = 2;
-        int DRIVETRAIN_RIGHT_VICTOR_2 = 4;
-        int DRIVETRAIN_PIGEON = -1;
+
+        int DRIVETRAIN_LEFT_TALON_1 = -1;
+        int DRIVETRAIN_LEFT_TALON_2 = -1;
+        int DRIVETRAIN_RIGHT_TALON_1 = -1;
+        int DRIVETRAIN_RIGHT_TALON_2 = -1;
+        int PIGEON_TALON = DRIVETRAIN_LEFT_TALON_1;
     }
 
     public interface DIO {
+
         int DRIVETRAIN_LEFT_ENCODER_POS = -1;
         int DRIVETRAIN_LEFT_ENCODER_NEG = -1;
         int DRIVETRAIN_RIGHT_ENCODER_POS = -1;
--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -1,7 +1,6 @@
 package frc.robot.Subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.revrobotics.ColorSensorV3;
 import com.spikes2212.command.drivetrains.TankDrivetrain;
 import com.spikes2212.control.FeedForwardSettings;
 import com.spikes2212.control.PIDSettings;
@@ -9,9 +8,7 @@ import com.spikes2212.dashboard.Namespace;
 import com.spikes2212.dashboard.RootNamespace;
 import com.spikes2212.util.PigeonWrapper;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.RobotMap;
 import frc.robot.utils.BustedMotorControllerGroup;
@@ -20,62 +17,72 @@ import java.util.function.Supplier;
 
 public class Drivetrain extends TankDrivetrain {
 
-    // The wheel moves 20.32 * PI (it's perimeter) each 4096 ticks.
-    private static final double DISTANCE_PER_PULSE = 20.32 * Math.PI / 4096;
-
-    // One side of the robot is faster than the other. To solve this we slow down one of the sides.
-    private static Supplier<Double> rightCorrection;
+    /**
+     * The wheel moves 20.32 * PI (it's perimeter) each 360 ticks.
+     * In meters.
+     */
+    private static final double DISTANCE_PER_PULSE = 20.32 * Math.PI / 360.0 / 100;
 
     private static Drivetrain instance;
 
-    public RootNamespace rootNamespace;
-    private Namespace encoderNamespace;
-    private Namespace leftColorSensorNamespace;
-    private Namespace rightColorSensorNamespace;
-    private Namespace PIDNamespace;
-    private Namespace FeedForwardNamespace;
+    private static final RootNamespace rootNamespace = new RootNamespace("drivetrain");
+    private static final Namespace encoderNamespace = rootNamespace.addChild("encoders");
+    private static final Namespace pigeonNamespace = rootNamespace.addChild("pigeon");
+    private static final Namespace PIDNamespace = rootNamespace.addChild("PID");
+    private static final Namespace FeedForwardNamespace = rootNamespace.addChild("feed forward");
+
+    /**
+     * One side of the robot is faster than the other. To solve this we slow down one of the sides.
+     */
+    private static final Supplier<Double> rightCorrection = rootNamespace.addConstantDouble("right correction", 1);
+    private static final Supplier<Double> leftCorrection = rootNamespace.addConstantDouble("left correction", 1);
 
     private final PigeonWrapper pigeon;
     private final Encoder leftEncoder, rightEncoder;
-    private final ColorSensorV3 rightColorSensor, leftColorSensor;
-    private final WPI_TalonSRX leftTalon1, leftTalon2, rightTalon1, rightTalon2;
 
-    private PIDSettings pidSettings;
-    private Supplier<Double> kP, kI, kD;
-    private FeedForwardSettings ffSettings;
-    private Supplier<Double> kV, kS, kA;
+    private final Supplier<Double> kP = PIDNamespace.addConstantDouble("kP", 0);
+    private final Supplier<Double> kI = PIDNamespace.addConstantDouble("kI", 0);
+    private final Supplier<Double> kD = PIDNamespace.addConstantDouble("kD", 0);
+    private final Supplier<Double> tolerance = PIDNamespace.addConstantDouble("tolerance", 0);
+    private final Supplier<Double> waitTime = PIDNamespace.addConstantDouble("wait time", 0);
+    private final PIDSettings pidSettings;
+
+    private final Supplier<Double> kV = FeedForwardNamespace.addConstantDouble("kV", 0);
+    private final Supplier<Double> kS = FeedForwardNamespace.addConstantDouble("kS", 0);
+    private final Supplier<Double> kA = FeedForwardNamespace.addConstantDouble("kA", 0);
+    private final FeedForwardSettings ffSettings;
 
     public static Drivetrain getInstance() {
         if (instance == null) {
-            instance = new Drivetrain(
-                    new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_LEFT_VICTOR_1),
-                    new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_LEFT_VICTOR_2),
-                    new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_RIGHT_VICTOR_1),
-                    new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_RIGHT_VICTOR_2)
+            WPI_TalonSRX pigeonTalon = new WPI_TalonSRX(RobotMap.CAN.PIGEON_TALON);
+            instance = new Drivetrain(new BustedMotorControllerGroup(
+                    leftCorrection,
+                    pigeonTalon, // @TODO: If you change the pigeon talon change this as well
+                    new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_LEFT_TALON_2)
+            ),
+                    new BustedMotorControllerGroup(
+                            rightCorrection,
+                            new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_RIGHT_TALON_1),
+                            new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_RIGHT_TALON_2)
+                    ),
+                    pigeonTalon
             );
         }
         return instance;
     }
 
-    private Drivetrain(WPI_TalonSRX leftTalon1, WPI_TalonSRX leftTalon2, WPI_TalonSRX rightTalon1, WPI_TalonSRX rightTalon2) {
-        super(new MotorControllerGroup(leftTalon1, leftTalon2), new BustedMotorControllerGroup(rightCorrection, rightTalon1, rightTalon2));
-
-        this.pigeon = new PigeonWrapper(RobotMap.CAN.DRIVETRAIN_PIGEON);
+    private Drivetrain(MotorControllerGroup leftMotors, BustedMotorControllerGroup rightMotors, WPI_TalonSRX pigeonTalon) {
+        super(leftMotors, rightMotors);
+        this.pigeon = new PigeonWrapper(pigeonTalon);
         this.leftEncoder = new Encoder(RobotMap.DIO.DRIVETRAIN_LEFT_ENCODER_POS, RobotMap.DIO.DRIVETRAIN_LEFT_ENCODER_NEG);
         this.rightEncoder = new Encoder(RobotMap.DIO.DRIVETRAIN_RIGHT_ENCODER_POS, RobotMap.DIO.DRIVETRAIN_RIGHT_ENCODER_NEG);
         this.leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
         this.rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
-        this.leftTalon1 = leftTalon1;
-        this.leftTalon2 = leftTalon2;
-        this.rightTalon1 = rightTalon1;
-        this.rightTalon2 = rightTalon2;
-        this.rightColorSensor = new ColorSensorV3(I2C.Port.kOnboard);
-        this.leftColorSensor = new ColorSensorV3(I2C.Port.kMXP);
-        this.pidSettings = new PIDSettings(kP, kI, kD);
-        this.ffSettings = new FeedForwardSettings(kS, kV, kA);
+        this.pidSettings = new PIDSettings(this.kP, this.kI, this.kD, this.tolerance, this.waitTime);
+        this.ffSettings = new FeedForwardSettings(this.kS, this.kV, this.kA);
     }
 
-    private void resetEncoders() {
+    public void resetEncoders() {
         leftEncoder.reset();
         rightEncoder.reset();
     }
@@ -85,41 +92,37 @@ public class Drivetrain extends TankDrivetrain {
         rootNamespace.update();
     }
 
-    public Color getLeftColor() {
-        return leftColorSensor.getColor();
-    }
-
-    public Color getRightColor() {
-        return rightColorSensor.getColor();
-    }
-
     public double getYaw() {
-        return pigeon.getYaw();
+        double yaw = pigeon.getYaw() % 360;
+        if (yaw > 180) yaw -= 360;
+        if (yaw < -180) yaw += 360;
+        return yaw;
     }
 
-    public double getRightTalon1Current() {
-        return rightTalon1.getStatorCurrent();
+    public double getRightDistance() {
+        return rightEncoder.getDistance();
     }
 
-    public double getLeftTalon1Current() {
-        return leftTalon1.getStatorCurrent();
+    public double getRightTicks() {
+        return rightEncoder.get();
+    }
+
+    public double getLeftDistance() {
+        return leftEncoder.getDistance();
+    }
+
+    public double getLeftTicks() {
+        return leftEncoder.get();
     }
 
     /**
-     * initialise namespaces and add sensor data to dashboard
+     * Initializes namespaces and adds sensor data to dashboard.
      */
     public void configureDashboard() {
-        rootNamespace = new RootNamespace("drivetrain");
-        leftColorSensorNamespace = rootNamespace.addChild("left color sensor");
-        rightColorSensorNamespace = rootNamespace.addChild("right color sensor");
-        encoderNamespace = rootNamespace.addChild("encoders");
-
-        rightCorrection = rootNamespace.addConstantDouble("right correction", 0);
-
-        encoderNamespace.putNumber("left ticks", this.leftEncoder::get);
-        encoderNamespace.putNumber("right ticks", this.rightEncoder::get);
-        encoderNamespace.putNumber("left distance", this.leftEncoder::getDistance);
-        encoderNamespace.putNumber("right distance", this.rightEncoder::getDistance);
+        encoderNamespace.putNumber("left ticks", leftEncoder::get);
+        encoderNamespace.putNumber("right ticks", rightEncoder::get);
+        encoderNamespace.putNumber("left distance", leftEncoder::getDistance);
+        encoderNamespace.putNumber("right distance", rightEncoder::getDistance);
         encoderNamespace.putData("reset encoders", new InstantCommand(this::resetEncoders) {
             @Override
             public boolean runsWhenDisabled() {
@@ -127,20 +130,12 @@ public class Drivetrain extends TankDrivetrain {
             }
         });
 
-        leftColorSensorNamespace.putNumber("left color sensor red", () -> this.getLeftColor().red);
-        leftColorSensorNamespace.putNumber("left color sensor blue", () -> this.getLeftColor().blue);
-        leftColorSensorNamespace.putNumber("left color sensor green", () -> this.getLeftColor().green);
-
-        rightColorSensorNamespace.putNumber("right color sensor red", () -> this.getRightColor().red);
-        rightColorSensorNamespace.putNumber("right color sensor blue", () -> this.getRightColor().blue);
-        rightColorSensorNamespace.putNumber("right color sensor green", () -> this.getRightColor().green);
-
-        kP = PIDNamespace.addConstantDouble("kP", 0);
-        kI = PIDNamespace.addConstantDouble("kI", 0);
-        kD = PIDNamespace.addConstantDouble("kD", 0);
-
-        kV = FeedForwardNamespace.addConstantDouble("kV", 0);
-        kS = FeedForwardNamespace.addConstantDouble("kS", 0);
-        kA = FeedForwardNamespace.addConstantDouble("kA", 0);
+        pigeonNamespace.putNumber("yaw", this::getYaw);
+        pigeonNamespace.putData("reset pigeon", new InstantCommand(pigeon::reset) {
+            @Override
+            public boolean runsWhenDisabled() {
+                return true;
+            }
+        });
     }
 }
