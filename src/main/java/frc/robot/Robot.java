f9e89c40216fbcf35c3045c5f14d649c811cb0c5
yotam <yotamyizhar@gmail.com>
2022-01-29 16:21:31 +0200

    no conflicts

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,6 +24,8 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotInit() {
+        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        // autonomous chooser on the dashboard.
         transfer = Transfer.getInstance();
     }
 
@@ -36,6 +38,10 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotPeriodic() {
+        // Runs the Scheduler. This is responsible for polling buttons, adding newly-scheduled
+        // commands, running already-scheduled commands, removing finished or interrupted commands,
+        // and running subsystem periodic() methods.  This must be called from the robot's periodic
+        // block in order for anything in the Command-based framework to work.
         CommandScheduler.getInstance().run();
     }
 
