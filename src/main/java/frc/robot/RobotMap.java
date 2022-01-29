6b69c8e533cf2bf9a333d70f042af0fe8f1492e4
yotam <yotamyizhar@gmail.com>
2022-01-29 14:47:20 +0200

    changed to motored generic subsystem

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,11 +4,9 @@ public class RobotMap {
     public interface CAN {
         int TRANSFER_STRAP_TALON_1 = -1;
         int TRANSFER_STRAP_TALON_2 = -1;
-        int TRANSFER_ENTRANCE_MOTOR = -1;
     }
 
     public interface DIO {
-        int TRANSFER_WHEEL_ENTRANCE_LIMIT_SWITCH = -1;
-        int TRANSFER_STRAP_LIGHT_SENSOR = -1;
+        int TRANSFER_ENTRANCE_LIGHT_SENSOR = -1;
     }
 }
--- src/main/java/frc/robot/Subsystems/Transfer.java
+++ src/main/java/frc/robot/Subsystems/Transfer.java
@@ -1,24 +1,19 @@
 package frc.robot.Subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.spikes2212.command.genericsubsystem.GenericSubsystem;
-import com.spikes2212.dashboard.RootNamespace;
+import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
 import com.spikes2212.util.Limelight;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.RobotMap;
-
-import java.util.function.Supplier;
 
 /**
  * transports cargo from the bottom to the top of the robot
  */
-public class Transfer extends GenericSubsystem {
+public class Transfer extends MotoredGenericSubsystem {
     // a light sensor that sends a signal while a cargo is held at the bottom of the timing straps
     private final DigitalInput entranceSensor;
 
     private final Limelight limelight;
-    private final MotorControllerGroup strapMotors;
 
     private static Transfer instance;
 
@@ -31,25 +26,11 @@ public class Transfer extends GenericSubsystem {
         return instance;
     }
 
-    private Transfer(WPI_TalonSRX strapTalon1, WPI_TalonSRX strapTalon2) {
-        this.strapMotors = new MotorControllerGroup(strapTalon1, strapTalon2);
-        this.entranceSensor = new DigitalInput(RobotMap.DIO.TRANSFER_STRAP_LIGHT_SENSOR);
+
+    public Transfer(WPI_TalonSRX talon1, WPI_TalonSRX talon2) {
+        super("transfer", talon1, talon2);
+        this.entranceSensor = new DigitalInput(RobotMap.DIO.TRANSFER_ENTRANCE_LIGHT_SENSOR);
         this.limelight = new Limelight();
-    }
-
-    @Override
-    protected void apply(double speed) {
-        strapMotors.set(speed);
-    }
-
-    @Override
-    public boolean canMove(double speed) {
-        return true;
-    }
-
-    @Override
-    public void stop() {
-        strapMotors.stopMotor();
     }
 
     public Limelight getLimelight() {
