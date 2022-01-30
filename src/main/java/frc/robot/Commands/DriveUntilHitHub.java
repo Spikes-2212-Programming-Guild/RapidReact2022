a429d6a19d443ba0b3d41a32b721b70094f36eca
yotam <yotamyizhar@gmail.com>
2022-01-30 19:43:01 +0200

    fixed documentation

--- src/main/java/frc/robot/Commands/DriveUntilHitHub.java
+++ src/main/java/frc/robot/Commands/DriveUntilHitHub.java
@@ -1,28 +1,31 @@
 package frc.robot.Commands;
 
 import com.spikes2212.command.drivetrains.commands.DriveArcade;
+import com.spikes2212.dashboard.RootNamespace;
 import frc.robot.Subsystems.Drivetrain;
 
 import java.util.function.Supplier;
 
 /**
- * drive the robot forward until it hits the hub. Robot needs to be pre aligned to the hub.
+ * Drives the robot forward until it hits the hub. Robot needs to be pre aligned to the hub.
  */
 public class DriveUntilHitHub extends DriveArcade {
 
-    // A value between the stall current and running current of the motors
-    private final Supplier<Double> STALL_CURRENT;
+    RootNamespace rootNamespace = new RootNamespace("drive until hit hub");
+    /**
+     * A value between the stall current and running current of the motors
+     */
+    private final Supplier<Double> STALL_CURRENT = rootNamespace.addConstantDouble("stall current", 0);
 
     private final Drivetrain drivetrain;
 
-    public DriveUntilHitHub(Drivetrain drivetrain, Supplier<Double> movementSpeed, Supplier<Double> stallCurrent) {
+    public DriveUntilHitHub(Drivetrain drivetrain, Supplier<Double> movementSpeed) {
         super(drivetrain, movementSpeed, () -> 0.0);
         this.drivetrain = drivetrain;
-        this.STALL_CURRENT = stallCurrent;
     }
 
     public DriveUntilHitHub(Drivetrain drivetrain, double movementSpeed, double stallCurrent) {
-        this(drivetrain, () -> movementSpeed, () -> stallCurrent);
+        this(drivetrain, () -> movementSpeed);
     }
 
     @Override
