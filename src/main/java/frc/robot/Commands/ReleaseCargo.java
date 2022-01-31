aa8b1e436a9c604ef4043a581979a642df36b893
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-01-31 21:22:04 +0200

    fixed a massive bug

--- src/main/java/frc/robot/Commands/ReleaseCargo.java
+++ src/main/java/frc/robot/Commands/ReleaseCargo.java
@@ -2,7 +2,6 @@ package frc.robot.Commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.Subsystems.Dispenser;
 import frc.robot.Subsystems.IntakeToTranfer;
 import frc.robot.Subsystems.Transfer;
 
