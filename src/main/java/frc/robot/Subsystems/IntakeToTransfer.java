b3c3ad4646961f930390ce73c2ad322151728e41
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-02-01 18:16:32 +0200

    removed doc

--- src/main/java/frc/robot/Subsystems/IntakeToTransfer.java
+++ src/main/java/frc/robot/Subsystems/IntakeToTransfer.java
@@ -4,30 +4,14 @@ import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
 
 public class IntakeToTransfer extends MotoredGenericSubsystem {
 
-    /**
-     * arbitrary speed value for compilability
-     */
     public static final double SPEED = 0.5;
 
-    /**
-     * arbitrary constractor for compilability
-     */
     public IntakeToTransfer() {
         super("intake to transfer");
     }
 
-    /**
-     * @return null for compilability
-     */
     public static IntakeToTransfer getInstance() {
         return null;
-    }
-
-    /**
-     * @return false for compilability
-     */
-    public boolean getLimit() {
-        return false;
     }
 
 }
--- src/main/java/frc/robot/Subsystems/Transfer.java
+++ src/main/java/frc/robot/Subsystems/Transfer.java
@@ -1,18 +1,11 @@
 package frc.robot.Subsystems;
 
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
-import com.spikes2212.control.FeedForwardSettings;
-import com.spikes2212.control.PIDSettings;
 
 public class Transfer extends MotoredGenericSubsystem {
-    /**
-     * arbitrary speed value for compilability
-     */
+
     public static final double SPEED = 0.5;
 
-    /**
-     * arbitrary constractor for compilability
-     */
     public Transfer() {
         super("transfer");
     }
@@ -21,17 +14,4 @@ public class Transfer extends MotoredGenericSubsystem {
         return null;
     }
 
-    /**
-     * @return null for compilability
-     */
-    public static PIDSettings getPIDSettings() {
-        return null;
-    }
-
-    /**
-     * @return null for compilability
-     */
-    public static FeedForwardSettings getFFSettings() {
-        return null;
-    }
 }
