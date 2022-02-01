308c6e067a461e97eb119a6afec0d60d61770478
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-02-01 18:52:10 +0200

    added move value as a shuffleboard constant

--- src/main/java/frc/robot/Commands/AimToHub.java
+++ src/main/java/frc/robot/Commands/AimToHub.java
@@ -1,9 +1,12 @@
 package frc.robot.Commands;
 
 import com.spikes2212.command.drivetrains.commands.DriveArcadeWithPID;
+import com.spikes2212.dashboard.RootNamespace;
 import com.spikes2212.util.Limelight;
 import frc.robot.Subsystems.Drivetrain;
 import frc.robot.Subsystems.Transfer;
+
+import java.util.function.Supplier;
 
 /**
  * A command which centers the robot on one of the reflective sprites that are on the hub.
@@ -11,13 +14,15 @@ import frc.robot.Subsystems.Transfer;
 public class AimToHub extends DriveArcadeWithPID {
 
     private static final int LIMELIGHT_PIPELINE = 1;
+    private static final RootNamespace aimToHubNS = new RootNamespace("aim to hub");
+    private static final Supplier<Double> MOVE_VALUE = aimToHubNS.addConstantDouble("move value", 0);
 
     private final Transfer transfer;
     private int initialPipeline;
     private final Limelight limelight;
 
     public AimToHub(Drivetrain drivetrain, Transfer transfer) {
-       super(drivetrain, transfer.getLimelight()::getHorizontalOffsetFromTarget, 0, 0,
+       super(drivetrain, transfer.getLimelight()::getHorizontalOffsetFromTarget, 0, MOVE_VALUE.get(),
                drivetrain.getPIDSettings(), drivetrain.getFFSettings());
        this.transfer = transfer;
        this.limelight = transfer.getLimelight();
