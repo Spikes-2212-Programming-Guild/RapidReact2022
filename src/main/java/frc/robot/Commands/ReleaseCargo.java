c7c581172ebe5492336aaa7e3129a9a7a04e8749
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-02-01 18:00:36 +0200

    compilability

--- src/main/java/frc/robot/Commands/ReleaseCargo.java
+++ src/main/java/frc/robot/Commands/ReleaseCargo.java
@@ -2,7 +2,7 @@ package frc.robot.Commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.Subsystems.IntakeToTranfer;
+import frc.robot.Subsystems.IntakeToTransfer;
 import frc.robot.Subsystems.Transfer;
 
 /**
@@ -12,10 +12,10 @@ public class ReleaseCargo extends ParallelCommandGroup {
 
     public ReleaseCargo() {
         Transfer transfer = Transfer.getInstance();
-        IntakeToTranfer intakeToTranfer = IntakeToTranfer.getInstance();
+        IntakeToTransfer intakeToTransfer = IntakeToTransfer.getInstance();
         addCommands(
                 new MoveGenericSubsystem(transfer, Transfer.SPEED),
-                new MoveGenericSubsystem(intakeToTranfer, IntakeToTranfer.SPEED)
+                new MoveGenericSubsystem(intakeToTransfer, IntakeToTransfer.SPEED)
         );
     }
 }
--- src/main/java/frc/robot/Subsystems/Transfer.java
+++ src/main/java/frc/robot/Subsystems/Transfer.java
@@ -5,14 +5,33 @@ import com.spikes2212.control.FeedForwardSettings;
 import com.spikes2212.control.PIDSettings;
 
 public class Transfer extends MotoredGenericSubsystem {
-    public static final double SPEED;
+    /**
+     * arbitrary speed value for compilability
+     */
+    public static final double SPEED = 0.5;
+
+    /**
+     * arbitrary constractor for compilability
+     */
+    public Transfer() {
+        super("transfer");
+    }
 
     public static Transfer getInstance() {
+        return null;
     }
 
+    /**
+     * @return null for compilability
+     */
     public static PIDSettings getPIDSettings() {
+        return null;
     }
 
+    /**
+     * @return null for compilability
+     */
     public static FeedForwardSettings getFFSettings() {
+        return null;
     }
 }
