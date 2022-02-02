a918c779609c9120fbf07ce07279ef2221fad950
OfriRosenbaum <ofriro1000>
2022-02-02 20:44:37 +0200

    deleted ClimberPlacer

--- src/main/java/frc/robot/Commands/Climb.java
+++ src/main/java/frc/robot/Commands/Climb.java
@@ -2,15 +2,13 @@ package frc.robot.Commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Subsystems.ClimberPlacer;
 import frc.robot.Subsystems.ClimberWinch;
 
 public class Climb extends SequentialCommandGroup {
 
     public Climb() {
         ClimberWinch climberWinch = ClimberWinch.getInstance();
-        ClimberPlacer climberPlacer = ClimberPlacer.getInstance();
-        addRequirements(climberWinch, climberPlacer);
+        addRequirements(climberWinch);
         addCommands(
                 new MoveGenericSubsystem(climberWinch, climberWinch.getUpSpeed()),
                 new MoveGenericSubsystem(climberWinch, climberWinch.getDownSpeed()) {
