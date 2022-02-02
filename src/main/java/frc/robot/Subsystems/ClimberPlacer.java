c85d7b9f458f37a5e468b154a6a1440f1dad69f3
yotam <yotamyizhar@gmail.com>
2022-02-02 20:07:58 +0200

    split ClimberPlacer into two instances

--- src/main/java/frc/robot/Subsystems/ClimberPlacer.java
+++ src/main/java/frc/robot/Subsystems/ClimberPlacer.java
@@ -5,46 +5,63 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.RobotMap;
 
+import java.util.function.Supplier;
+
 /**
  * Controls the position of the climber.
  */
 public class ClimberPlacer extends MotoredGenericSubsystem {
 
-    private static final double MIN_SPEED = -0.6;
-    private static final double MAX_SPEED = 0.6;
+    private final Supplier<Double> STALL_CURRENT = rootNamespace.addConstantDouble("stall current", 0);
 
-    private static ClimberPlacer instance;
+    private static final double MIN_SPEED = -0.6;
+    private static final double MAX_SPEED =  0.6;
+
+    private static ClimberPlacer rightInstance, leftInstance;
 
     private final DigitalInput frontLimit;
     private final DigitalInput backLimit;
-    private static WPI_TalonSRX leftPlacer;
-    private static WPI_TalonSRX rightPlacer;
+    private final WPI_TalonSRX placer;
 
-    public static ClimberPlacer getInstance() {
-        if (instance == null) {
-            leftPlacer = new WPI_TalonSRX(RobotMap.CAN.PLACER_TALON_LEFT);
-            rightPlacer = new WPI_TalonSRX(RobotMap.CAN.PLACER_TALON_RIGHT);
-            instance = new ClimberPlacer(leftPlacer, rightPlacer);
+    private boolean stalling = false;
+    private boolean passedFirstStall = false;
+
+    public static ClimberPlacer getRightInstance() {
+        if (rightInstance == null) {
+            rightInstance = new ClimberPlacer(new WPI_TalonSRX(RobotMap.CAN.PLACER_TALON_RIGHT));
         }
-        return instance;
+        return rightInstance;
     }
 
-    private ClimberPlacer(WPI_TalonSRX leftPlacer, WPI_TalonSRX rightPlacer) {
-        super(MIN_SPEED, MAX_SPEED, "climber placer", leftPlacer, rightPlacer);
+    public static ClimberPlacer getLeftInstance() {
+        if (leftInstance == null) {
+            leftInstance = new ClimberPlacer(new WPI_TalonSRX(RobotMap.CAN.PLACER_TALON_LEFT));
+        }
+        return leftInstance;
+    }
+
+    private ClimberPlacer(WPI_TalonSRX placer) {
+        super(MIN_SPEED, MAX_SPEED, "right climber placer", placer);
+        this.placer = placer;
         this.frontLimit = new DigitalInput(RobotMap.DIO.PLACER_LIMIT_FRONT);
         this.backLimit = new DigitalInput(RobotMap.DIO.PLACER_LIMIT_BACK);
     }
 
     @Override
     public boolean canMove(double speed) {
-        return !(ClimberWinch.getInstance().isHooked() || speed > 0 && frontLimit.get() || speed < 0 && backLimit.get());
+        if (!passedFirstStall && placer.getStatorCurrent() > STALL_CURRENT.get())
+            stalling = true;
+        else if (!passedFirstStall && stalling && placer.getStatorCurrent() <= STALL_CURRENT.get()) {
+            stalling = false;
+            passedFirstStall = true;
+        } else if (passedFirstStall && placer.getStatorCurrent() >= STALL_CURRENT.get())
+            stalling = true;
+
+        return !(ClimberWinch.getInstance().isHooked() || speed > 0 && frontLimit.get() || speed < 0 && backLimit.get()) &&
+                !(passedFirstStall && stalling);
     }
 
-    public WPI_TalonSRX getLeftTalon() {
-        return leftPlacer;
-    }
-
-    public WPI_TalonSRX getRightTalon() {
-        return rightPlacer;
+    public WPI_TalonSRX getPlacer() {
+        return placer;
     }
 }
