22de38d62a51fc523c9782973d5c38b1d0c689c7
Yotam Yizhar <yotamyizhar@gmail.com>
2022-02-07 15:26:07 +0200

    fixed formatting

--- src/main/java/frc/robot/Subsystems/ClimberPlacer.java
+++ src/main/java/frc/robot/Subsystems/ClimberPlacer.java
@@ -1,9 +1,8 @@
 package frc.robot.Subsystems;
 
-import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Timer;
 import frc.robot.RobotMap;
 
 import java.util.function.Supplier;
@@ -15,41 +14,46 @@ public class ClimberPlacer extends MotoredGenericSubsystem {
 
     private static final double MIN_SPEED = -0.6;
     private static final double MAX_SPEED = 0.6;
-
     private static ClimberPlacer rightInstance, leftInstance;
 
-    private final Supplier<Double> MOVEMENT_SPEED = rootNamespace.addConstantDouble("movement speed", 0);
+    private final Supplier<Double> DROP_SPEED = rootNamespace.addConstantDouble("drop speed", 0);
 
-    private final DigitalInput frontLimit;
-    private final DigitalInput backLimit;
+    private static final DigitalInput frontLimit = new DigitalInput(RobotMap.DIO.PLACER_LIMIT_FRONT);
+    private static final DigitalInput backLimit = new DigitalInput(RobotMap.DIO.PLACER_LIMIT_BACK);
     private final WPI_TalonSRX placer;
 
     public static ClimberPlacer getRightInstance() {
         if (rightInstance == null) {
-            rightInstance = new ClimberPlacer(new WPI_TalonSRX(RobotMap.CAN.PLACER_TALON_RIGHT));
+            rightInstance = new ClimberPlacer(new WPI_TalonSRX(RobotMap.CAN.PLACER_TALON_RIGHT), "right");
         }
         return rightInstance;
     }
 
     public static ClimberPlacer getLeftInstance() {
         if (leftInstance == null) {
-            leftInstance = new ClimberPlacer(new WPI_TalonSRX(RobotMap.CAN.PLACER_TALON_LEFT));
+            leftInstance = new ClimberPlacer(new WPI_TalonSRX(RobotMap.CAN.PLACER_TALON_LEFT), "left");
         }
         return leftInstance;
     }
 
-    private ClimberPlacer(WPI_TalonSRX placer) {
-        super(MIN_SPEED, MAX_SPEED, "right climber placer", placer);
+    private ClimberPlacer(WPI_TalonSRX placer, String side) {
+        super(MIN_SPEED, MAX_SPEED, side + " climber placer", placer);
         this.placer = placer;
-        this.frontLimit = new DigitalInput(RobotMap.DIO.PLACER_LIMIT_FRONT);
-        this.backLimit = new DigitalInput(RobotMap.DIO.PLACER_LIMIT_BACK);
     }
 
     public WPI_TalonSRX getPlacer() {
         return placer;
     }
 
-    public Supplier<Double> getUpSpeed() {
-        return MOVEMENT_SPEED;
+    public Supplier<Double> getDropSpeed() {
+        return DROP_SPEED;
+    }
+
+    public boolean getFrontLimit() {
+        return ClimberPlacer.frontLimit.get();
+    }
+
+    public boolean getBackLimit() {
+        return ClimberPlacer.backLimit.get();
     }
 }
