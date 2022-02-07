40d7d4950b0f7424b3f0f549c76dd9745aa5952e
Yotam Yizhar <yotamyizhar@gmail.com>
2022-02-07 18:43:14 +0200

    changed location of startTime initialization

--- src/main/java/frc/robot/Commands/DropPlacer.java
+++ src/main/java/frc/robot/Commands/DropPlacer.java
@@ -1,27 +1,38 @@
 package frc.robot.Commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.dashboard.RootNamespace;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Subsystems.ClimberPlacer;
+
+import java.util.function.Supplier;
 
 public class DropPlacer extends MoveGenericSubsystem {
 
     private final ClimberPlacer placer;
 
-    private final double startTime;
+    private static final RootNamespace rootNamespace = new RootNamespace("drop placers");
+    public static final Supplier<Double> initialWaitTime = rootNamespace.addConstantDouble("initial wait time", 0);
+
+    private double startTime;
 
     public DropPlacer(ClimberPlacer placer) {
         super(placer, placer.getDropSpeed());
         this.placer = placer;
-        this.startTime = Timer.getFPGATimestamp();
     }
 
-    private boolean oneSecondPassed() {
-        return Timer.getFPGATimestamp() - startTime > 1;
+    private boolean initialTimePassed() {
+        return Timer.getFPGATimestamp() - startTime > initialWaitTime.get();
+    }
+
+    @Override
+    public void initialize() {
+        startTime = Timer.getFPGATimestamp();
+        super.initialize();
     }
 
     @Override
     public boolean isFinished() {
-        return super.isFinished() && placer.isStalling() && oneSecondPassed();
+        return super.isFinished() && placer.isStalling() && initialTimePassed();
     }
 }
