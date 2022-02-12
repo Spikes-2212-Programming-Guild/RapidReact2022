28ef7d575b0c68aa3dd90b5d7711d5c29eb4d9eb
tal-sitton <53299816+tal-sitton@users.noreply.github.com>
2022-02-12 13:32:07 +0200

    Update Transfer.java

--- src/main/java/frc/robot/Subsystems/Transfer.java
+++ src/main/java/frc/robot/Subsystems/Transfer.java
@@ -1,19 +1,4 @@
 package frc.robot.Subsystems;
 
-import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-
-public class Transfer extends MotoredGenericSubsystem {
-
-    public static Transfer getInstance() {
-        return null;
-    }
-
-    private Transfer(String namespaceName, MotorController... motorControllers) {
-        super(namespaceName, motorControllers);
-    }
-
-    public boolean getStrapEntranceSensor() {
-        return false;
-    }
+public class Transfer {
 }
