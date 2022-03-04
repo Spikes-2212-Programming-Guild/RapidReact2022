7b01672612600856a5684d55aea58a5db21d3d17
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-03-04 13:26:25 +0200

    made DriveUntilHitHub not stupid

--- src/main/java/frc/robot/commands/DriveUntilHitHub.java
+++ src/main/java/frc/robot/commands/DriveUntilHitHub.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import com.spikes2212.command.drivetrains.commands.DriveArcade;
 import com.spikes2212.dashboard.RootNamespace;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
 
 import java.util.function.Supplier;
@@ -9,7 +10,7 @@ import java.util.function.Supplier;
 /**
  * Drives the robot forward until it hits the hub. Robot needs to be pre aligned to the hub.
  */
-public class DriveUntilHitHub extends DriveArcade {
+public class DriveUntilHitHub extends SequentialCommandGroup {
 
     private static final RootNamespace rootNamespace = new RootNamespace("drive until hit hub");
 
@@ -24,22 +25,19 @@ public class DriveUntilHitHub extends DriveArcade {
             = rootNamespace.addConstantDouble("get over stall timeout", 0.2);
 
     public DriveUntilHitHub(Drivetrain drivetrain) {
-        super(drivetrain, MOVEMENT_SPEED, () -> 0.0);
-    }
-
-    @Override
-    public void initialize() {
-        new DriveArcade(tankDrivetrain, MOVEMENT_SPEED, () -> 0.0) {
-            @Override
-            public void end(boolean interrupted) {
-            }
-        }.withTimeout(GET_OVER_STALL_TIMEOUT.get());
-    }
-
-    @Override
-    public boolean isFinished() {
-        Drivetrain drivetrain = (Drivetrain) tankDrivetrain;
-        return Math.abs(drivetrain.getRightTalon().getStatorCurrent()) > STALL_CURRENT.get() &&
-               Math.abs(drivetrain.getLeftTalon().getStatorCurrent()) > STALL_CURRENT.get();
+        super(
+                new DriveArcade(drivetrain, MOVEMENT_SPEED, () -> 0.0) {
+                    @Override
+                    public void end(boolean interrupted) {}
+                }.withTimeout(GET_OVER_STALL_TIMEOUT.get()),
+                new DriveArcade(drivetrain, MOVEMENT_SPEED, () -> 0.0) {
+                    @Override
+                    public boolean isFinished() {
+                        Drivetrain drivetrain = (Drivetrain) tankDrivetrain;
+                        return Math.abs(drivetrain.getRightTalon().getStatorCurrent()) > STALL_CURRENT.get() &&
+                                Math.abs(drivetrain.getLeftTalon().getStatorCurrent()) > STALL_CURRENT.get();
+                    }
+                }
+        );
     }
 }
