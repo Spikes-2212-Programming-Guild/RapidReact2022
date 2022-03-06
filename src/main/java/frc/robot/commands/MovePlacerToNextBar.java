9cbdd575c2dc588c655915421a1550f908f2d3a7
Yotam Yizhar <yotamyizhar@gmail.com>
2022-03-06 18:56:46 +0200

    beautified MovePlacerToNextBar

--- src/main/java/frc/robot/commands/MovePlacerToNextBar.java
+++ src/main/java/frc/robot/commands/MovePlacerToNextBar.java
@@ -2,36 +2,30 @@ package frc.robot.commands;
 
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import com.spikes2212.dashboard.RootNamespace;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ClimberPlacer;
 
 import java.util.function.Supplier;
 
-public class MovePlacerToNextBar extends MoveGenericSubsystem {
+public class MovePlacerToNextBar extends SequentialCommandGroup {
 
-    private final ClimberPlacer placer;
+    private static final RootNamespace rootNamespace = new RootNamespace("move placers to next bar");
 
-    private static final RootNamespace rootNamespace = new RootNamespace("drop placers");
-    public static final Supplier<Double> INITIAL_WAIT_TIME = rootNamespace.addConstantDouble("initial wait time", 0.25);
-
-    private double startTime;
+    public static final Supplier<Double> GET_OVER_STALL_TIMEOUT =
+            rootNamespace.addConstantDouble("get over stall timeout", 0.2);
 
     public MovePlacerToNextBar(ClimberPlacer placer) {
-        super(placer, placer.RAISE_SPEED);
-        this.placer = placer;
-    }
-
-    @Override
-    public void initialize() {
-        startTime = Timer.getFPGATimestamp();
-    }
-
-    @Override
-    public boolean isFinished() {
-        return super.isFinished() || (placer.isStalling() && initialTimePassed());
-    }
-
-    private boolean initialTimePassed() {
-        return Timer.getFPGATimestamp() - startTime > INITIAL_WAIT_TIME.get();
+        super(
+                new MoveGenericSubsystem(placer, placer.RAISE_SPEED) {
+                    @Override
+                    public void end(boolean interrupted) {}
+                }.withTimeout(GET_OVER_STALL_TIMEOUT.get()),
+                new MoveGenericSubsystem(placer, placer.RAISE_SPEED) {
+                    @Override
+                    public boolean isFinished() {
+                        return placer.isStalling();
+                    }
+                }
+        );
     }
 }
