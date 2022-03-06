68609d2fcda27ed56558521d5ae5ece14cd231f1
Yotam Yizhar <yotamyizhar@gmail.com>
2022-03-06 22:08:11 +0200

    present simple

--- src/main/java/frc/robot/commands/MoveToNextBar.java
+++ src/main/java/frc/robot/commands/MoveToNextBar.java
@@ -12,11 +12,11 @@ import java.util.function.Supplier;
 /**
  * Moves the robot to the next bar. <br>
  * Steps: <br>
- * 1. Move the winch up to release the hooks from the bar.<br>
- * 2. Move the placers downwards.<br>
- * 3. Extend the winch to it's full extent.<br>
- * 4. Raise the placers until they hit the next bar and go into a stall.<br>
- * 5. Close the winch to move the robot to the next bar.<br>
+ * 1. Moves the winch up to release the hooks from the bar.<br>
+ * 2. Moves the placers downwards.<br>
+ * 3. Extends the winch to it's full extent.<br>
+ * 4. Raises the placers until they hit the next bar and go into a stall.<br>
+ * 5. Closes the winch to move the robot to the next bar.<br>
  * Must start the command <b>ONLY</b> when you are already on a bar.
  */
 public class MoveToNextBar extends SequentialCommandGroup {
