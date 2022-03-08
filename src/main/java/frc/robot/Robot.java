91c8b03aabff830c91e95fbc6996b1ce013b7a3f
OfriRosenbaum <ofriro1000@gmail.com>
2022-03-08 10:57:30 +0200

    deleted IntakePlacerDefaultCommand

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -53,8 +53,6 @@ public class Robot extends TimedRobot {
         rootNamespace.putData("drive forward", new DriveArcade(drivetrain, 0.5, 0));
         rootNamespace.putData("drive backward", new DriveArcade(drivetrain, -0.5, 0));
         rootNamespace.putData("move to cargo", new MoveToCargo(drivetrain, MoveToCargo.CARGO_MOVE_VALUE));
-
-        intakePlacer.setDefaultCommand(new IntakePlacerDefaultCommand());
     }
 
     /**
