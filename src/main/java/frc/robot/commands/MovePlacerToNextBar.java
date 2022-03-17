b7b9fd91f2dae9ba661403209b9caf7e84e948a7
OfriRosenbaum <ofriro1000@gmail.com>
2022-03-17 18:58:12 +0200

    fixed MovePlacerToNextBar so it will fit the new hasHitNextBar method

--- src/main/java/frc/robot/commands/MovePlacerToNextBar.java
+++ src/main/java/frc/robot/commands/MovePlacerToNextBar.java
@@ -7,32 +7,15 @@ import frc.robot.subsystems.ClimberPlacer;
 
 import java.util.function.Supplier;
 
-public class MovePlacerToNextBar extends SequentialCommandGroup {
-
-    private static final RootNamespace rootNamespace = new RootNamespace("move placers to next bar");
-
-    public static final Supplier<Double> INITIAL_MOVE_DURATION =
-            rootNamespace.addConstantDouble("initial move duration", 0.2);
+public class MovePlacerToNextBar extends MoveGenericSubsystem {
 
     public MovePlacerToNextBar(ClimberPlacer placer) {
-        super(
-                /*
-                  When the placer starts moving, its initial speed will be close to 0, and thus the placer will be
-                  considered in stall. As a result, we first want to move the placer in order for it to obtain
-                  velocity.
-                 */
-                new MoveGenericSubsystem(placer, placer.RAISE_SPEED) {
-                    @Override
-                    public void end(boolean interrupted) {
-                    }
-                }.withTimeout(INITIAL_MOVE_DURATION.get()),
-                new MoveGenericSubsystem(placer, placer.RAISE_SPEED) {
-                    @Override
-                    public boolean isFinished() {
-                        // Checks if the placer hit the next bar
-                        return placer.hasHitNextBar();
-                    }
-                }
-        );
+        super(placer, placer.RAISE_SPEED);
+    }
+
+    @Override
+    public boolean isFinished() {
+        ClimberPlacer climberPlacer = (ClimberPlacer) subsystem;
+        return climberPlacer.hasHitNextBar();
     }
 }
