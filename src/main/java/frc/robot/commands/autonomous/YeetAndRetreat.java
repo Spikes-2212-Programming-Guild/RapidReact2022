d651f8068ad3055bef690fcd98c85aceba1bb4c9
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-03-24 17:34:29 +0200

    removed magic

--- src/main/java/frc/robot/commands/autonomous/YeetAndRetreat.java
+++ src/main/java/frc/robot/commands/autonomous/YeetAndRetreat.java
@@ -12,11 +12,12 @@ public class YeetAndRetreat extends SequentialCommandGroup {
     public static final double DRIVE_SPEED = 0.7;
     public static final double DRIVE_ROTATE = 0;
     public static final double DRIVE_DURATION = 1.2212;
+    public static final double WAIT_DURATION = 3;
 
     public YeetAndRetreat() {
         super(
                 new ReleaseCargo().withTimeout(RELEASE_DURATION),
-                new WaitCommand(3),
+                new WaitCommand(WAIT_DURATION),
                 new DriveArcade(Drivetrain.getInstance(), DRIVE_SPEED, DRIVE_ROTATE).withTimeout(DRIVE_DURATION)
         );
     }
