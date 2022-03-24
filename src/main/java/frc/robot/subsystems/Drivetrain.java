85ef841becfdf543747b0c9565c1a1b2b5a96b31
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-03-24 20:30:45 +0200

    removed encoders

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -17,15 +17,8 @@ import java.util.function.Supplier;
 
 public class Drivetrain extends TankDrivetrain {
 
-    /**
-     * The wheel moves 15.24 * PI (it's perimeter) each 360 ticks (in meters).
-     */
-    private static final double DISTANCE_PER_PULSE = 15.24 * Math.PI / 360.0 / 100;
-
     private static Drivetrain instance;
 
-    private final Namespace encoderNamespace = rootNamespace.addChild("encoders");
-//    private final Namespace encodersPIDNamespace = encoderNamespace.addChild("encoders pid");
     private final Namespace gyroNamespace = rootNamespace.addChild("gyro");
     private final Namespace gyroPIDNamespace = gyroNamespace.addChild("gyro pid");
     private final Namespace cameraPIDNamespace = rootNamespace.addChild("camera pid");
@@ -40,7 +33,6 @@ public class Drivetrain extends TankDrivetrain {
 
     private final PigeonWrapper pigeon;
     private final WPI_TalonSRX leftTalon, rightTalon;
-    private final Encoder leftEncoder, rightEncoder;
 
     private final Supplier<Double> kPGyro = gyroPIDNamespace.addConstantDouble("kP", 0.017);
     private final Supplier<Double> kIGyro = gyroPIDNamespace.addConstantDouble("kI", 0.0028);
@@ -48,13 +40,6 @@ public class Drivetrain extends TankDrivetrain {
     private final Supplier<Double> toleranceGyro = gyroPIDNamespace.addConstantDouble("tolerance", 5);
     private final Supplier<Double> waitTimeGyro = gyroPIDNamespace.addConstantDouble("wait time", 0.1);
     private final PIDSettings pidSettingsGyro;
-
-//    private final Supplier<Double> kPEncoders = encodersPIDNamespace.addConstantDouble("kP", 0);
-//    private final Supplier<Double> kIEncoders = encodersPIDNamespace.addConstantDouble("kI", 0);
-//    private final Supplier<Double> kDEncoders = encodersPIDNamespace.addConstantDouble("kD", 0);
-//    private final Supplier<Double> toleranceEncoders = encodersPIDNamespace.addConstantDouble("tolerance", 0);
-//    private final Supplier<Double> waitTimeEncoders = encodersPIDNamespace.addConstantDouble("wait time", 0);
-//    private final PIDSettings pidSettingsEncoders;
 
     private final Supplier<Double> kPCamera = cameraPIDNamespace.addConstantDouble("kP", 0.0065);
     private final Supplier<Double> kICamera = cameraPIDNamespace.addConstantDouble("kI", 0);
@@ -96,22 +81,11 @@ public class Drivetrain extends TankDrivetrain {
         this.pigeon = new PigeonWrapper(pigeonTalon);
         this.leftTalon = leftTalon;
         this.rightTalon = rightTalon;
-        this.leftEncoder = new Encoder(RobotMap.DIO.DRIVETRAIN_LEFT_ENCODER_POS, RobotMap.DIO.DRIVETRAIN_LEFT_ENCODER_NEG);
-        this.rightEncoder = new Encoder(RobotMap.DIO.DRIVETRAIN_RIGHT_ENCODER_POS, RobotMap.DIO.DRIVETRAIN_RIGHT_ENCODER_NEG);
-        this.leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
-        this.rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
         this.pidSettingsGyro = new PIDSettings(this.kPGyro, this.kIGyro, this.kDGyro, this.toleranceGyro,
                 this.waitTimeGyro);
-//        this.pidSettingsEncoders = new PIDSettings(this.kPEncoders, this.kIEncoders, this.kDEncoders,
-//                this.toleranceEncoders, this.waitTimeEncoders);
         this.pidSettingsCamera = new PIDSettings(this.kPCamera, this.kICamera, this.kDCamera,
                 this.toleranceCamera, this.waitTimeCamera);
         this.ffSettings = new FeedForwardSettings(this.kS, this.kV, this.kA);
-    }
-
-    public void resetEncoders() {
-        leftEncoder.reset();
-        rightEncoder.reset();
     }
 
     public void resetPigeon() {
@@ -128,22 +102,6 @@ public class Drivetrain extends TankDrivetrain {
         if (yaw > 180) yaw -= 360;
         if (yaw < -180) yaw += 360;
         return yaw;
-    }
-
-    public double getRightDistance() {
-        return rightEncoder.getDistance();
-    }
-
-    public double getRightTicks() {
-        return rightEncoder.get();
-    }
-
-    public double getLeftDistance() {
-        return leftEncoder.getDistance();
-    }
-
-    public double getLeftTicks() {
-        return leftEncoder.get();
     }
 
     public WPI_TalonSRX getLeftTalon() {
@@ -170,16 +128,6 @@ public class Drivetrain extends TankDrivetrain {
      * Initializes namespaces and adds sensor data to dashboard.
      */
     public void configureDashboard() {
-        encoderNamespace.putNumber("left ticks", leftEncoder::get);
-        encoderNamespace.putNumber("right ticks", rightEncoder::get);
-        encoderNamespace.putNumber("left distance", leftEncoder::getDistance);
-        encoderNamespace.putNumber("right distance", rightEncoder::getDistance);
-        encoderNamespace.putData("reset encoders", new InstantCommand(this::resetEncoders) {
-            @Override
-            public boolean runsWhenDisabled() {
-                return true;
-            }
-        });
         gyroNamespace.putNumber("yaw", this::getYaw);
         gyroNamespace.putData("reset pigeon", new InstantCommand(this::resetPigeon) {
             @Override
@@ -191,3 +139,4 @@ public class Drivetrain extends TankDrivetrain {
         rootNamespace.putNumber("left talon current", leftTalon::getStatorCurrent);
     }
 }
+
