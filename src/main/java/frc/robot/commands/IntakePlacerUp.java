ac76dae1bfa371c3cbbabee44abbbafdd3cc57fd
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-03-24 14:09:46 +0200

    added doc to IntakePlacerUp

--- src/main/java/frc/robot/commands/IntakePlacerUp.java
+++ src/main/java/frc/robot/commands/IntakePlacerUp.java
@@ -4,6 +4,11 @@ import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.subsystems.IntakePlacer;
 
+/**
+ * Moves the IntakePlacer up until: <br>
+ * 1. The upper limit is hit. <br>
+ * 2. {@value TIMEOUT} seconds have passed since the upper limit was hit.
+ */
 public class IntakePlacerUp extends MoveGenericSubsystem {
 
     private static final double TIMEOUT = 0.3;
