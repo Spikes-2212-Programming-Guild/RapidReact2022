3e0b3e48e39606dfcd1a2253fd93b74bfaa7d8ca
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-03-24 20:31:19 +0200

    fixed compilation error

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -100,7 +100,6 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousInit() {
         rootNamespace.putBoolean("is in auto", true);
-        drivetrain.resetEncoders();
         drivetrain.resetPigeon();
 //        new GyroAutonomous().schedule();
         new YeetAndRetreat().schedule();
