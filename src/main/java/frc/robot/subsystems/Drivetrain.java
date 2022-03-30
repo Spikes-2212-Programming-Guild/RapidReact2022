3486f061a167e81425392a58cab087f3008eda2e
Yotam Yizhar <yotamyizhar@gmail.com>
2022-03-30 10:01:40 +0300

    added seekHub() method and ghost getLimelight() method

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,6 +9,7 @@ import com.spikes2212.control.PIDSettings;
 import com.spikes2212.dashboard.Namespace;
 import com.spikes2212.dashboard.RootNamespace;
 import com.spikes2212.util.BustedMotorControllerGroup;
+import com.spikes2212.util.Limelight;
 import com.spikes2212.util.PigeonWrapper;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -124,6 +125,10 @@ public class Drivetrain extends TankDrivetrain {
 
     public FeedForwardSettings getFFSettings() {
         return ffSettings;
+    }
+
+    public Limelight getLimelight() {
+        return null;
     }
 
     /**
