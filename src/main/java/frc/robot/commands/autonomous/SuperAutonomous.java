f3c2ee9d4da192b7c47d44bb936edadb1fa7fc57
OfriRosenbaum <ofriro1000@gmail.com>
2022-03-30 17:27:57 +0300

    added commands to constructor

--- src/main/java/frc/robot/commands/autonomous/SuperAutonomous.java
+++ src/main/java/frc/robot/commands/autonomous/SuperAutonomous.java
@@ -5,10 +5,14 @@ import com.spikes2212.command.drivetrains.commands.DriveTankWithPID;
 import com.spikes2212.dashboard.Namespace;
 import com.spikes2212.dashboard.RootNamespace;
 import com.spikes2212.util.Limelight;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.commands.DriveUntilHitHub;
+import frc.robot.commands.IntakeCargo;
 import frc.robot.commands.MoveToCargo;
+import frc.robot.commands.ReleaseCargo;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.IntakePlacer;
+import frc.robot.subsystems.IntakeToTransfer;
 
 import java.util.function.Supplier;
 
@@ -18,12 +22,50 @@ public class SuperAutonomous extends SequentialCommandGroup {
     private static final Supplier<Double> SEEK_ROTATE_VALUE = rootNamespace.addConstantDouble("seek rotate value", 0.5);
     private static final Supplier<Double> SEEK_HUB_TOLERANCE = rootNamespace.addConstantDouble("seek hub tolerance", 18);
 
+    public static final double FIRST_RELEASE_CARGO_TIMEOUT = 1;
+
+    public static final Supplier<Double> MOVE_TO_CARGO_SPEED = () -> 0.7;
+
+    public static final double INTAKE_FIRST_CARGO_TIMEOUT = 2.5;
+    public static final double INTAKE_SECOND_CARGO_TIMEOUT = 2;
+
     public static final double SEEK_CARGO_TOLERANCE = 90;
+    public static final double SEEK_CARGO_TIMEOUT = 1;
+
+    public static final double RETURN_BY_ENCODERS_TIMEOUT = 1;
+
+    public static final double SEEK_HUB_TIMEOUT = 1;
+
+    public static final double DRIVE_UNTIL_HIT_HUB_TIMEOUT = 2;
+
+    public static final double SECOND_RELEASE_CARGO_TIMEOUT = 2;
 
     private final Drivetrain drivetrain;
 
     public SuperAutonomous() {
         this.drivetrain = Drivetrain.getInstance();
+        addCommands(
+                new ParallelCommandGroup(
+                        new InstantCommand(() -> IntakePlacer.getInstance().setServoAngle(IntakePlacer.SERVO_TARGET_ANGLE.get())),
+                        new ReleaseCargo().withTimeout(FIRST_RELEASE_CARGO_TIMEOUT)
+                ),
+                moveToCargoWithIntake().withTimeout(INTAKE_FIRST_CARGO_TIMEOUT),
+                seekCargo().withTimeout(SEEK_CARGO_TIMEOUT),
+                moveToCargoWithIntake().withTimeout(INTAKE_SECOND_CARGO_TIMEOUT),
+                returnByEncoders().withTimeout(RETURN_BY_ENCODERS_TIMEOUT),
+                seekHub().withTimeout(SEEK_HUB_TIMEOUT),
+                new DriveUntilHitHub(drivetrain).withTimeout(DRIVE_UNTIL_HIT_HUB_TIMEOUT),
+                new ReleaseCargo().withTimeout(SECOND_RELEASE_CARGO_TIMEOUT)
+        );
+    }
+
+    private ParallelCommandGroup moveToCargoWithIntake() {
+        return new ParallelCommandGroup(
+                new IntakeCargo(false),
+                new SequentialCommandGroup(
+                        new MoveToCargo(drivetrain, MOVE_TO_CARGO_SPEED),
+                        new DriveArcade(drivetrain, MOVE_TO_CARGO_SPEED, () -> 0.0)
+                ).withInterrupt(IntakeToTransfer.getInstance()::getLimit));
     }
 
     private DriveTankWithPID returnByEncoders() {
@@ -36,7 +78,7 @@ public class SuperAutonomous extends SequentialCommandGroup {
         Limelight limelight = drivetrain.getLimelight();
         return new DriveArcade(drivetrain, () -> 0.0, SEEK_ROTATE_VALUE).withInterrupt(() ->
                 -SEEK_HUB_TOLERANCE.get() <= limelight.getHorizontalOffsetFromTarget() &&
-                limelight.getHorizontalOffsetFromTarget() <= SEEK_HUB_TOLERANCE.get());
+                        limelight.getHorizontalOffsetFromTarget() <= SEEK_HUB_TOLERANCE.get());
     }
 
     private DriveArcade seekCargo() {
