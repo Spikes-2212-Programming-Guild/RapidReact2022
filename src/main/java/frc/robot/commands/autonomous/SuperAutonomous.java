f8e8290a171676cba571098f11cb5453ada0e16b
Yotam Yizhar <yotamyizhar@gmail.com>
2022-03-30 11:58:19 +0300

    cahnged to 0.5

--- src/main/java/frc/robot/commands/autonomous/SuperAutonomous.java
+++ src/main/java/frc/robot/commands/autonomous/SuperAutonomous.java
@@ -15,7 +15,7 @@ import java.util.function.Supplier;
 public class SuperAutonomous extends SequentialCommandGroup {
 
     private static final RootNamespace rootNamespace = new RootNamespace("super auto");
-    private static final Supplier<Double> SEEK_HUB_ROTATE_VALUE = rootNamespace.addConstantDouble("seek hub rotate value", 0.4);
+    private static final Supplier<Double> SEEK_HUB_ROTATE_VALUE = rootNamespace.addConstantDouble("seek hub rotate value", 0.5);
     private static final Supplier<Double> SEEK_HUB_TOLERANCE = rootNamespace.addConstantDouble("seek hub tolerance", 18);
     public static final double SEEK_CARGO_TOLERANCE = 90;
 
